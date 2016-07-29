88bdee8482536c717accc73894e96ced0bf159b9
rvind92 <rvind92@gmail.com>
2016-07-29 10:21:54 +0800

    POST user and POST user/login, added all module dependencies, and set up DB

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3030;
 var todos = [];
 var todoNextId = 1;
 
@@ -11,6 +13,39 @@ app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
 	response.send('Tanand Web Server Root');
+});
+
+app.post('/users', function(request, response) {
+	var body = _.pick(request.body, 'email', 'password');
+
+	db.user.create(body).then(function (user) {
+		response.json(user.toPublicJSON());
+	}, function (e) {
+		response.status(400).json(e);
+		console.log(e);
+	});
+});
+
+app.post('/users/login', function(request, response) {
+	var body = _.pick(request.body, 'email', 'password');
+
+	if(typeof body.email !== 'string' || typeof body.password !== 'string') {
+		return response.status(400).send();
+	}
+
+	db.user.findOne({
+		where: {
+			email: body.email
+		}
+	}).then(function(user) {
+		if(!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+			return response.status(401).send();
+		}
+
+		response.json(user.toPublicJSON());
+	}, function(e) {
+		response.status(500).send();
+	});
 });
 
 app.listen(PORT, function() {
