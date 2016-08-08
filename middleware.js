56b1c453d260767678b529c46768306ae3e7c720
Aravind Krishna <rvind92@gmail.com>
2016-08-08 09:26:57 +0800

    Refactored requireAuthentication in middleware.js to search user by token instead

--- middleware.js
+++ middleware.js
@@ -2,12 +2,24 @@ module.exports = function(db) {
 
 	return {
 		requireAuthentication: function(request, response, next) {
-			var token = request.get('Auth');
+			var token = request.get('Auth') || '';
 
-			db.user.findByToken(token).then(function(user) {
+			db.token.findOne({
+				where: {
+					tokenHash: cryptojs.MD5(token).toString()
+				}
+			}).then(function(tokenInstance) {
+				if(!tokenInstance) {
+					throw new Error();
+				}
+
+				request.token = tokenInstance;
+				return db.user.findByToken(token);
+
+			}).then(function(user) {
 				request.user = user;
 				next();
-			}, function() {
+			}).catch(function() {
 				response.status(401).send();
 			});
 		},
