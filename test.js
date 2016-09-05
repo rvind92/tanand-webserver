a69ce7128a892ef0b2dc3a839f243ffad52b47d5
Aravind Krishna <rvind92@gmail.com>
2016-09-05 15:24:26 +0800

    Added webconsole project to public directory

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
+var name = 'Aravind Radha Krishna';
+console.log('Name :' + name);
+
+var trimName = (name.replace(/ /g, '').toLowerCase());
+console.log('\nName after trim: ' + trimName);
+
+var lowerCaseName = trimName.toLowerCase();
+console.log('\nName to lowercase: ' + lowerCaseName);
+
 // // var jsonData = {
 // //    "version": "1.1",
 // //    "devices": [
@@ -44,93 +53,93 @@
 // console.log(timeInMs);
 // console.log(convert);
 
-module.exports = function(sequelize, DataTypes) {
+// module.exports = function(sequelize, DataTypes) {
 
-    var Author = sequelize.define('Author', {
+//     var Author = sequelize.define('Author', {
 
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        firstName: {
-            type: DataTypes.STRING
-        },
-        lastName: {
-            type: DataTypes.STRING
-        }
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             autoIncrement: true,
+//             primaryKey: true
+//         },
+//         firstName: {
+//             type: DataTypes.STRING
+//         },
+//         lastName: {
+//             type: DataTypes.STRING
+//         }
 
-    });
+//     });
 
-    var Book = sequelize.define('Book', {
+//     var Book = sequelize.define('Book', {
 
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        title: {
-            type: DataTypes.STRING
-        }
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             autoIncrement: true,
+//             primaryKey: true
+//         },
+//         title: {
+//             type: DataTypes.STRING
+//         }
 
-    });
+//     });
 
-    var firstAuthor;
-    var secondAuthor;
+//     var firstAuthor;
+//     var secondAuthor;
 
-    Author.hasMany(Book)
-    Book.belongsTo(Author)
+//     Author.hasMany(Book)
+//     Book.belongsTo(Author)
 
-    Author.sync({ 
-    	force: true 
-    }).then(function() {
-            return Book.sync({ 
-            	force: true 
-            });
-        }).then(function() {
-            return Author.create({
-            	firstName: 'Aravind', 
-            	lastName: 'Krishna'
-            });
-        }).then(function(author1) {
-            firstAuthor = author1;
-            return Author.create({
-            	firstName: 'Hsien', 
-            	lastName: 'Kun'
-            });
-        }).then(function(author2) {
-            secondAuthor = author2
-            return Book.create({
-            	AuthorId: firstAuthor.id, 
-            	title: 'A simple book'
-            });
-        }).then(function() {
-            return Book.create({
-            	AuthorId: firstAuthor.id, 
-            	title: 'Another book'
-            });
-        }).then(function() {
-            return Book.create({
-            	AuthorId: secondAuthor.id, 
-            	title: 'Some other book'
-            });
-        }).then(function() {
-            // This is the part you're after.
-            return Book.findAll({
-                where: {
-                   'Authors.lastName': 'Krishna'
-                },
-                include: [
-                    {
-                    	model: Author, 
-                    	as: Author.tableName
-                    }
-                ]
-            });
-        }).then(function(books) { 
-            console.log('There are ' + books.length + ' books by Aravind Krishna')
-        });
-    }
+//     Author.sync({ 
+//     	force: true 
+//     }).then(function() {
+//             return Book.sync({ 
+//             	force: true 
+//             });
+//         }).then(function() {
+//             return Author.create({
+//             	firstName: 'Aravind', 
+//             	lastName: 'Krishna'
+//             });
+//         }).then(function(author1) {
+//             firstAuthor = author1;
+//             return Author.create({
+//             	firstName: 'Hsien', 
+//             	lastName: 'Kun'
+//             });
+//         }).then(function(author2) {
+//             secondAuthor = author2
+//             return Book.create({
+//             	AuthorId: firstAuthor.id, 
+//             	title: 'A simple book'
+//             });
+//         }).then(function() {
+//             return Book.create({
+//             	AuthorId: firstAuthor.id, 
+//             	title: 'Another book'
+//             });
+//         }).then(function() {
+//             return Book.create({
+//             	AuthorId: secondAuthor.id, 
+//             	title: 'Some other book'
+//             });
+//         }).then(function() {
+//             // This is the part you're after.
+//             return Book.findAll({
+//                 where: {
+//                    'Authors.lastName': 'Krishna'
+//                 },
+//                 include: [
+//                     {
+//                     	model: Author, 
+//                     	as: Author.tableName
+//                     }
+//                 ]
+//             });
+//         }).then(function(books) { 
+//             console.log('There are ' + books.length + ' books by Aravind Krishna')
+//         });
+//     }
 
