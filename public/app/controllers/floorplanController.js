2768f9a7e3440927a1031058e19db9928629dbf7
rvind92 <rvind92@gmail.com>
2016-09-08 11:39:28 +0800

    Fixed floorplan.html and floorplanController.js.

--- public/app/controllers/floorplanController.js
+++ public/app/controllers/floorplanController.js
@@ -1,93 +1,110 @@
-(function() {
+(function() { 
 
     var FloorplanController = function($scope, firebaseFactory) {
 
-       var sites = [];
+        var sites = [];
+        var metadata;
+        var file;
 
-       var sitesLoaded = firebase.database().ref('buildingList');
-       sitesLoaded.on('value', function(snapshot) {
-           snapshot.forEach(function(siteKey) {
-            sites.push({
-              name: siteKey.key
-          });
+        var sitesLoaded = firebase.database().ref('buildingList');
+        sitesLoaded.on('value', function(snapshot) {
+            snapshot.forEach(function(siteKey) {
+                sites.push({
+                    name: siteKey.key
+                });
+            });
+            $scope.$apply();
+            console.log('This is: ' + JSON.stringify(sites));
+
+        }, function() {
+            alert('No site(s) available at the moment.');
         });
-           $scope.$apply();
-           console.log('This is: ' + JSON.stringify(sites));
 
-       }, function() {
-        alert('No site(s) available at the moment.');
-    });
-
-       $scope.site = {
-        site : null,
-        availableOptions: sites
-    };          
-
-    $scope.updateBuildingSelect = function(value) {
-        var buildings = [];
-        console.log(value + ' has been selected!');
-        var buildingsLoaded = firebase.database().ref('buildingList').child(value);
-        buildingsLoaded.on('value', function(snapshot) {
-           snapshot.forEach(function(buildingKey) {
-               buildings.push({
-                   name: buildingKey.key
-               });
-           });
-           $scope.$apply();
-           console.log('This is: ' + JSON.stringify(buildings));
-       }, function(e) {
-           alert('No building(s) available at the moment.');
-       });
-
-        $scope.building = {
-           site: null,
-           availableOptions: buildings
-       };
-   }
-
-   $scope.loadFile = function ($input) {
-    if ($input.files && $input.files[0]) {
-        var reader = new FileReader();
-
-        console.log(reader);
-
-        reader.onload = function (e) {
-            $('#blah')
-            .attr('src', e.target.result)
-            .width(300)
-            .height(180);
+        $scope.site = {
+            site : null,
+            availableOptions: sites
         };
 
-        reader.readAsDataURL($input.files[0]);
-    }
-};
+        document.getElementById('file').addEventListener('change', handleFileSelect, false);
 
-$scope.reset = function(){
-    document.getElementById('blah').src = "";
-};
+        var storageRef = firebase.storage().ref();
+        function handleFileSelect(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            file = evt.target.files[0];
+            metadata = {
+                'contentType': file.type
+            };
+            console.log(metadata);
+        }
+        
+        $scope.updateBuildingSelect = function(value) {
+            var buildings = [];
+            console.log(value + ' has been selected!');
+            var buildingsLoaded = firebase.database().ref('buildingList').child(value);
+            buildingsLoaded.on('value', function(snapshot) {
+                snapshot.forEach(function(buildingKey) {
+                    buildings.push({
+                        name: buildingKey.key
+                    });
+                });
+                console.log('This is: ' + JSON.stringify(buildings));
+            }, function(e) {
+                alert('No building(s) available at the moment.');
+            });
 
-        // $scope.onFloorplanCreate = function() {
+            $scope.building = {
+                site: null,
+                availableOptions: buildings
+            };
+        }
 
-        //     var floorObj = $scope.form;
+        $scope.loadFile = function ($input) {
+            if ($input.files && $input.files[0]) {
+                var reader = new FileReader();
 
-        //     var siteKey = floorObj.site;
-        //     var buildKey = floorObj.building;
-        //     var floorId = floorObj.floorplanid;
-        //     var floorName = floorObj.floorplanname;
-        //     var floorUrl;
+                console.log(reader);
 
-        //     firebaseFactory.setFloorplan(siteKey, buildKey, floorid, floorname, floorUrl).then(function() {
-        //         alert(floorname + ' successfully added!');
-        //     }, function(e) {
-        //         alert('This function cannot be performed at the moment!');
-        //     });
+                reader.onload = function (e) {
+                    $('#blah')
+                    .attr('src', e.target.result)
+                    .width(300)
+                    .height(180);
+                };
 
-        // }
+                reader.readAsDataURL($input.files[0]);
+            }
+        };
 
+        $scope.reset = function() {
+            document.getElementById('blah').src = "";
+        };
+
+        $scope.onFloorplanCreate = function() {
+
+            var floorObj = $scope.form;
+
+            var siteKey = floorObj.site;
+            var buildKey = floorObj.building;
+            var floorId = floorObj.floorplanid;
+            var floorName = floorObj.floorplanname;
+            var floorUrl;
+
+            storageRef.child(siteKey + '/' + buildKey + '/' + floorName + '/' + file.name).put(file, metadata).then(function(snapshot) {
+                floorUrl = snapshot.metadata.downloadURLs[0];
+                console.log('File available at', floorUrl);
+                firebaseFactory.setFloorplan(siteKey, buildKey, floorId, floorName, floorUrl).then(function() {
+                }, function(e) {
+                    alert('This function cannot be performed at the moment!');
+                });
+            }).catch(function(error) {
+                console.error('Upload failed:', error);
+            });
+        }
     }
 
     FloorplanController.$inject = ['$scope', 'firebaseFactory'];
-    
+
     angular.module('tanandApp').controller('FloorplanController', FloorplanController);
-    
+
 }());
